4e876eb8b37f3dc336de7a1469634cc7f223a719
Bartek Banach <thebartekbanach.dev@gmail.com>
2024-05-05 19:18:34 +0200

    refactor: remove old go sdk

--- go/handler.go
+++
@@ -1,9 +0,0 @@
-package directmq
-
-type SubscriptionHandler func(publish Publish, subscription Subscribe)
-
-type SubscriptionOnceHandler func(publish Publish, subscription SubscribeOnce)
-
-type PublicationHandler interface {
-	HandlePublish(publication Publish)
-}
