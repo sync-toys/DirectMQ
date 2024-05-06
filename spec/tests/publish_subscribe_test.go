70b828ee7b614370af6261546b110222055d7bb7
Bartek Banach <thebartekbanach.dev@gmail.com>
2024-05-06 20:08:28 +0200

    test: use pair test bench in pub sub test

--- spec/tests/publish_subscribe_test.go
+++ spec/tests/publish_subscribe_test.go
@@ -2,7 +2,6 @@ package dmqtests
 
 import (
 	"io"
-	"net/url"
 
 	"github.com/fatih/color"
 	. "github.com/onsi/ginkgo/v2"
@@ -10,175 +9,68 @@ import (
 	directmq "github.com/sync-toys/DirectMQ/sdk/go"
 	dmqspec "github.com/sync-toys/DirectMQ/spec"
 	dmqspecagent "github.com/sync-toys/DirectMQ/spec/agent_api"
+	dmqspecagents "github.com/sync-toys/DirectMQ/spec/agents"
+	testbench "github.com/sync-toys/DirectMQ/spec/test_bench"
 )
 
 var _ = Describe("Pubsub", func() {
-	logTest := dmqspec.CreateLogger("test", func(message string, writer io.Writer) {
+	log := dmqspec.CreateLogger("test", func(message string, writer io.Writer) {
 		color.New(color.FgGreen).Fprintln(writer, message)
-	})
-
-	logComm := dmqspec.CreateLogger("comm", func(message string, writer io.Writer) {
-		color.New(color.FgHiYellow).Fprintln(writer, message)
-	})
-
-	logMaster := dmqspec.CreateLogger("master", func(message string, writer io.Writer) {
-		color.New(color.FgHiBlue).Fprintln(writer, message)
-	})
-	logSalve := dmqspec.CreateLogger("salve", func(message string, writer io.Writer) {
-		color.New(color.FgHiBlue).Fprintln(writer, message)
-	})
-
-	forwardFrom, _ := url.Parse("http://localhost:8081/")
-	forwardTo, _ := url.Parse("ws://localhost:8080/")
-	forwarder := dmqspecagent.NewWebsocketForwarder(forwardFrom, forwardTo)
-	forwarder.OnMessage(func(message string, sender, receiver *url.URL) {
-		senderAgent := "master"
-		receiverAgent := "salve"
-		if sender.Port() == "8081" {
-			senderAgent = "salve"
-			receiverAgent = "master"
-		}
-
-		logComm("%s->%s message: %s", senderAgent, receiverAgent, message)
-	})
-
-	master := dmqspecagent.NewUniversalAgent()
-	master.OnLogEntry(func(entry string) {
-		logMaster(entry)
-	})
-	master.OnFatal(func(fatal dmqspecagent.FatalNotification) {
-		logMaster("FATAL: %s\n", fatal.Err)
-		Fail(fatal.Err)
-	})
-
-	salve := dmqspecagent.NewUniversalAgent()
-	salve.OnLogEntry(func(entry string) {
-		logSalve(entry)
-	})
-	salve.OnFatal(func(fatal dmqspecagent.FatalNotification) {
-		logSalve("FATAL: %s\n", fatal.Err)
-		Fail(fatal.Err)
-	})
-
-	var masterExec dmqspecagent.UniversalSpawn = dmqspecagent.UniversalSpawn{
-		// ExecutablePath: "dlv",
-		// Arguments:      []string{"exec", "./agents/go/go-agent.exe", "--headless", "--listen=:2345", "--api-version=2"},
-
-		ExecutablePath: "../agents/go/bin/go-agent",
-		Arguments:      []string{},
-	}
-
-	var salveExec dmqspecagent.UniversalSpawn = dmqspecagent.UniversalSpawn{
-		// ExecutablePath: "dlv",
-		// Arguments:      []string{"exec", "./agents/go/go-agent.exe", "--headless", "--listen=:2345", "--api-version=2"},
-
-		ExecutablePath: "../agents/go/bin/go-agent",
-		Arguments:      []string{},
-	}
-
-	BeforeEach(func() {
-		go forwarder.StartWebsocketForwarder()
-	})
-
-	AfterEach(func() {
-		logTest("stopping agents and forwarder")
-
-		logTest("stopping master agent")
-		master.Stop(dmqspecagent.StopCommand{
-			Reason: "test ended",
-		})
-
-		logTest("stopping salve agent")
-		salve.Stop(dmqspecagent.StopCommand{
-			Reason: "test ended",
-		})
-
-		logTest("aborting forwarder")
-		forwarder.Abort()
-
-		logTest("agents and forwarder stopped gracefully")
 	})
 
 	When("agent A publishes a message", func() {
 		It("should be received by agent B", func() {
-			masterReady := make(chan struct{}, 1)
-			masterConnected := make(chan struct{}, 1)
+			bench := testbench.NewGinkgoPairTopoTestBench(
+				testbench.PairTopoTestBenchConfig{
+					MasterSpawn: dmqspecagents.GolangAgent("master", dmqspecagents.NO_DEBUGGING),
+					SalveSpawn:  dmqspecagents.GolangAgent("salve", dmqspecagents.NO_DEBUGGING),
 
-			salveReady := make(chan struct{}, 1)
-			salveConnected := make(chan struct{}, 1)
+					MasterTTL:            32,
+					MasterMaxMessageSize: directmq.NO_MAX_MESSAGE_SIZE,
 
-			master.OnReady(func(_ dmqspecagent.ReadyNotification) {
-				logTest("master ready")
-				masterReady <- struct{}{}
-			})
+					SalveTTL:            32,
+					SalveMaxMessageSize: directmq.NO_MAX_MESSAGE_SIZE,
 
-			master.OnConnectionEstablished(func(notification dmqspecagent.ConnectionEstablishedNotification) {
-				logTest("master connected")
-				masterConnected <- struct{}{}
-			})
+					LogMasterToSalveCommunication: true,
+					LogSalveToMasterCommunication: true,
 
-			salve.OnReady(func(_ dmqspecagent.ReadyNotification) {
-				logTest("salve ready")
-				salveReady <- struct{}{}
-			})
+					LogMasterLogs: true,
+					LogSalveLogs:  true,
 
-			salve.OnConnectionEstablished(func(notification dmqspecagent.ConnectionEstablishedNotification) {
-				logTest("salve connected")
-				salveConnected <- struct{}{}
-			})
+					DisableAllLogs: false,
+				},
+			)
 
-			logTest("listening on master agent")
-			master.Listen(dmqspecagent.ListenCommand{
-				TTL:            32,
-				Address:        "http://localhost:8080/",
-				AsClientId:     "master",
-				MaxMessageSize: directmq.NO_MAX_MESSAGE_SIZE,
-			}, masterExec)
-
-			<-masterReady
-
-			logTest("connecting from salve agent")
-			salve.Connect(dmqspecagent.ConnectCommand{
-				TTL:            32,
-				Address:        "ws://localhost:8081/",
-				AsClientId:     "salve",
-				MaxMessageSize: directmq.NO_MAX_MESSAGE_SIZE,
-			}, salveExec)
-
-			<-salveReady
-
-			logTest("waiting for master connection to be established")
-			<-masterConnected
-
-			logTest("waiting for salve connection to be established")
-			<-salveConnected
+			bench.Start()
+			defer bench.Stop("test ended")
+			defer bench.SnapshotRecording("pubsub")
 
 			subscriptionPropagated := make(chan struct{}, 1)
-			master.OnSubscription(func(subscription dmqspecagent.OnSubscriptionNotification) {
+			bench.Master.OnSubscription(func(subscription dmqspecagent.OnSubscriptionNotification) {
 				subscriptionPropagated <- struct{}{}
 			})
 
-			logTest("subscribing to test topic from salve")
-			salve.Subscribe(dmqspecagent.SubscribeTopicCommand{
+			log("subscribing to test topic from salve")
+			bench.Salve.Subscribe(dmqspecagent.SubscribeTopicCommand{
 				Topic: "test",
 			})
 
-			logTest("waiting for subscription to be propagated to master")
+			log("waiting for subscription to be propagated to master")
 			<-subscriptionPropagated
 
 			result := make(chan []byte)
-			salve.OnMessageReceived(func(message dmqspecagent.MessageReceivedNotification) {
+			bench.Salve.OnMessageReceived(func(message dmqspecagent.MessageReceivedNotification) {
 				result <- message.Payload
 			})
 
-			logTest("publishing message from master on test topic")
-			master.Publish(dmqspecagent.PublishCommand{
+			log("publishing message from master on test topic")
+			bench.Master.Publish(dmqspecagent.PublishCommand{
 				Topic:            "test",
 				DeliveryStrategy: directmq.AT_LEAST_ONCE,
 				Payload:          []byte("Hello, World!"),
 			})
 
-			logTest("waiting for message to be received by salve")
+			log("waiting for message to be received by salve")
 			Expect(<-result).To(Equal([]byte("Hello, World!")))
 		})
 	})
