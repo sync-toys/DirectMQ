7be42a18a33d1968c2c04e256dd6891f6ceb8b96
Bartek Banach <thebartekbanach.dev@gmail.com>
2024-05-07 18:49:33 +0200

    test: add basic communication tests

--- spec/tests/publish_subscribe_test.go
+++ spec/tests/publish_subscribe_test.go
@@ -18,60 +18,168 @@ var _ = Describe("Pubsub", func() {
 		color.New(color.FgGreen).Fprintln(writer, message)
 	})
 
-	When("agent A publishes a message", func() {
-		It("should be received by agent B", func() {
-			bench := testbench.NewGinkgoPairTopoTestBench(
-				testbench.PairTopoTestBenchConfig{
-					MasterSpawn: dmqspecagents.GolangAgent("master", dmqspecagents.NO_DEBUGGING),
-					SalveSpawn:  dmqspecagents.GolangAgent("salve", dmqspecagents.NO_DEBUGGING),
+	Context("basic communication", func() {
+		When("master publishes a message on simple topic", func() {
+			var bench *testbench.PairTopoTestBench
 
-					MasterTTL:            32,
-					MasterMaxMessageSize: directmq.NO_MAX_MESSAGE_SIZE,
+			BeforeEach(func() {
+				bench = testbench.NewGinkgoPairTopoTestBench(
+					testbench.PairTopoTestBenchConfig{
+						MasterSpawn: dmqspecagents.GolangAgent("master", dmqspecagents.NO_DEBUGGING),
+						SalveSpawn:  dmqspecagents.GolangAgent("salve", dmqspecagents.NO_DEBUGGING),
 
-					SalveTTL:            32,
-					SalveMaxMessageSize: directmq.NO_MAX_MESSAGE_SIZE,
+						MasterTTL:            directmq.DEFAULT_TTL,
+						MasterMaxMessageSize: directmq.NO_MAX_MESSAGE_SIZE,
 
-					LogMasterToSalveCommunication: true,
-					LogSalveToMasterCommunication: true,
+						SalveTTL:            directmq.DEFAULT_TTL,
+						SalveMaxMessageSize: directmq.NO_MAX_MESSAGE_SIZE,
 
-					LogMasterLogs: true,
-					LogSalveLogs:  true,
+						LogMasterToSalveCommunication: true,
+						LogSalveToMasterCommunication: true,
 
-					DisableAllLogs: false,
-				},
-			)
+						LogMasterLogs: true,
+						LogSalveLogs:  true,
 
-			bench.Start()
-			defer bench.Stop("test ended")
-			defer bench.SnapshotRecording("pubsub")
+						DisableAllLogs: false,
+					},
+				)
 
-			subscriptionPropagated := make(chan struct{}, 1)
-			bench.Master.OnSubscription(func(subscription dmqspecagent.OnSubscriptionNotification) {
-				subscriptionPropagated <- struct{}{}
+				bench.Start()
 			})
 
-			log("subscribing to test topic from salve")
-			bench.Salve.Subscribe(dmqspecagent.SubscribeTopicCommand{
-				Topic: "test",
+			AfterEach(func() {
+				bench.Stop("test ended")
 			})
 
-			log("waiting for subscription to be propagated to master")
-			<-subscriptionPropagated
+			It("should be received by salve subscription", func() {
+				defer bench.SnapshotRecording("pubsub-mtos")
 
-			result := make(chan []byte)
-			bench.Salve.OnMessageReceived(func(message dmqspecagent.MessageReceivedNotification) {
-				result <- message.Payload
+				subscriptionPropagatedToMaster := make(chan struct{}, 1)
+				bench.Master.OnSubscription(func(subscription dmqspecagent.OnSubscriptionNotification) {
+					subscriptionPropagatedToMaster <- struct{}{}
+				})
+
+				log("subscribing to test topic from salve")
+				bench.Salve.Subscribe(dmqspecagent.SubscribeTopicCommand{
+					Topic: "test",
+				})
+
+				log("waiting for subscription to be propagated to master")
+				<-subscriptionPropagatedToMaster
+
+				messageReceivedBySalve := make(chan []byte)
+				bench.Salve.OnMessageReceived(func(message dmqspecagent.MessageReceivedNotification) {
+					messageReceivedBySalve <- message.Payload
+				})
+
+				log("publishing message from master on test topic")
+				bench.Master.Publish(dmqspecagent.PublishCommand{
+					Topic:            "test",
+					DeliveryStrategy: directmq.AT_LEAST_ONCE,
+					Payload:          []byte("Hello, World!"),
+				})
+
+				log("waiting for message to be received by salve")
+				Expect(<-messageReceivedBySalve).To(Equal([]byte("Hello, World!")))
 			})
 
-			log("publishing message from master on test topic")
-			bench.Master.Publish(dmqspecagent.PublishCommand{
-				Topic:            "test",
-				DeliveryStrategy: directmq.AT_LEAST_ONCE,
-				Payload:          []byte("Hello, World!"),
+			It("should be received by master subscription", func() {
+				defer bench.SnapshotRecording("pubsub-mtom")
+
+				subscriptionPropagatedToSalve := make(chan struct{}, 1)
+				bench.Salve.OnSubscription(func(subscription dmqspecagent.OnSubscriptionNotification) {
+					subscriptionPropagatedToSalve <- struct{}{}
+				})
+
+				log("subscribing to test topic from master")
+				bench.Master.Subscribe(dmqspecagent.SubscribeTopicCommand{
+					Topic: "test",
+				})
+
+				log("waiting for subscription to be propagated to salve")
+				<-subscriptionPropagatedToSalve
+
+				messageReceivedByMaster := make(chan []byte)
+				bench.Master.OnMessageReceived(func(message dmqspecagent.MessageReceivedNotification) {
+					messageReceivedByMaster <- message.Payload
+				})
+
+				log("publishing message from master on test topic")
+				bench.Master.Publish(dmqspecagent.PublishCommand{
+					Topic:            "test",
+					DeliveryStrategy: directmq.AT_LEAST_ONCE,
+					Payload:          []byte("Hello, World!"),
+				})
+
+				log("waiting for message to be received by master")
+				Expect(<-messageReceivedByMaster).To(Equal([]byte("Hello, World!")))
 			})
 
-			log("waiting for message to be received by salve")
-			Expect(<-result).To(Equal([]byte("Hello, World!")))
+			It("should be received all subscribers", func() {
+				defer bench.SnapshotRecording("pubsub-all")
+
+				subscriptionPropagatedToSalve := make(chan struct{}, 1)
+				bench.Salve.OnSubscription(func(subscription dmqspecagent.OnSubscriptionNotification) {
+					subscriptionPropagatedToSalve <- struct{}{}
+				})
+
+				log("subscribing to test topic from master")
+				bench.Master.Subscribe(dmqspecagent.SubscribeTopicCommand{
+					Topic: "test",
+				})
+
+				log("waiting for subscription to be propagated to salve")
+				<-subscriptionPropagatedToSalve
+
+				subscriptionPropagatedToMaster := make(chan struct{}, 1)
+				bench.Master.OnSubscription(func(subscription dmqspecagent.OnSubscriptionNotification) {
+					subscriptionPropagatedToMaster <- struct{}{}
+				})
+
+				log("subscribing to test topic from salve")
+				bench.Salve.Subscribe(dmqspecagent.SubscribeTopicCommand{
+					Topic: "test",
+				})
+
+				log("waiting for subscription to be propagated to master")
+				<-subscriptionPropagatedToMaster
+
+				messageReceivedByMaster := make(chan []byte)
+				bench.Master.OnMessageReceived(func(message dmqspecagent.MessageReceivedNotification) {
+					messageReceivedByMaster <- message.Payload
+				})
+
+				messageReceivedBySalve := make(chan []byte)
+				bench.Salve.OnMessageReceived(func(message dmqspecagent.MessageReceivedNotification) {
+					messageReceivedBySalve <- message.Payload
+				})
+
+				log("publishing message from master on test topic")
+				bench.Master.Publish(dmqspecagent.PublishCommand{
+					Topic:            "test",
+					DeliveryStrategy: directmq.AT_LEAST_ONCE,
+					Payload:          []byte("Hello, World! (master)"),
+				})
+
+				log("waiting for message to be received by master")
+				Expect(<-messageReceivedByMaster).To(Equal([]byte("Hello, World! (master)")))
+
+				log("waiting for message to be received by salve")
+				Expect(<-messageReceivedBySalve).To(Equal([]byte("Hello, World! (master)")))
+
+				log("publishing message from salve on test topic")
+				bench.Salve.Publish(dmqspecagent.PublishCommand{
+					Topic:            "test",
+					DeliveryStrategy: directmq.AT_LEAST_ONCE,
+					Payload:          []byte("Hello, World! (salve)"),
+				})
+
+				log("waiting for message to be received by salve")
+				Expect(<-messageReceivedBySalve).To(Equal([]byte("Hello, World! (salve)")))
+
+				log("waiting for message to be received by master")
+				Expect(<-messageReceivedByMaster).To(Equal([]byte("Hello, World! (salve)")))
+			})
 		})
 	})
 })
