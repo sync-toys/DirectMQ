429d4e08583292d32f82efdb8905cd66131c056d
Bartek Banach <thebartekbanach.dev@gmail.com>
2024-05-26 15:14:47 +0200

    test: remove verbose output from ginkgo to improve tests speed

--- spec/run-tests.sh
+++ spec/run-tests.sh
@@ -4,6 +4,7 @@ echo Building golang SDK test agent
 cd ./agents/go && go build -gcflags="all=-N -l" -o bin/go-agent && cd ../../
 
 echo Running specification tests
-cd ./tests && ginkgo -v && cd ../
+# cd ./tests && ginkgo -v --focus "" && cd ../
+cd ./tests && ginkgo && cd ../
 
 echo Done
