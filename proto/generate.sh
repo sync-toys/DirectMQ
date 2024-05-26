d72353673604575a639bd611d55dccdb5fa5a07c
Bartek Banach <thebartekbanach.dev@gmail.com>
2024-05-26 15:34:30 +0200

    build: fix protocol generation

--- proto/generate.sh
+++ proto/generate.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 rm -rf /sdk/go/protocol
-# rm -rf /sdk/cpp/protocol
+rm -rf /sdk/cpp/protocol
 # rm -rf /sdk/ts/protocol
 
-buf lint src
-buf generate src
+buf lint --path ./directmq
+buf generate --path ./directmq
 
 mkdir -p /sdk/go/protocol
-# mkdir -p /sdk/cpp/protocol
+mkdir -p /sdk/cpp/protocol
 # mkdir -p /sdk/ts/protocol
