2793ccb0ff96ce652e6de5dbf88d893186e97fa1
Bartek Banach <thebartekbanach.dev@gmail.com>
2024-05-28 14:21:03 +0200

    fix: move cpp library generation under lib directory

--- proto/generate.sh
+++ proto/generate.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 rm -rf /sdk/go/protocol
-rm -rf /sdk/cpp/protocol
+rm -rf /sdk/cpp/lib/protocol
 # rm -rf /sdk/ts/protocol
 
 buf lint --path ./directmq
 buf generate --path ./directmq
 
 mkdir -p /sdk/go/protocol
-mkdir -p /sdk/cpp/protocol
+mkdir -p /sdk/cpp/lib/protocol
 # mkdir -p /sdk/ts/protocol
