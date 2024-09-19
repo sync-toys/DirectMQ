0bf8584cb2724fa07d5c3707854d5f33f4ff0a40
Bartek Banach <thebartekbanach.dev@gmail.com>
2024-09-19 11:10:07 +0000

    refactor: move directmq embedded proto generated assets to dir with correct naming convention

--- proto/generate.sh
+++ proto/generate.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 rm -rf /sdk/go/protocol
-rm -rf /sdk/cpp/lib/dmq-proto-embedded
+rm -rf /sdk/cpp/lib/dmq_proto_embedded
 # rm -rf /sdk/ts/protocol
 
 mkdir -p /sdk/go/protocol
-mkdir -p /sdk/cpp/lib/dmq-proto-embedded
+mkdir -p /sdk/cpp/lib/dmq_proto_embedded
 # mkdir -p /sdk/ts/protocol
 
 buf lint --path ./directmq
 buf generate --path ./directmq
-cp CMakeLists.txt /sdk/cpp/lib/dmq-proto-embedded
+cp CMakeLists.txt /sdk/cpp/lib/dmq_proto_embedded
