2b9dfa67fc15ea2ceecdca24a8dcf2804c4ef991
Bartek Banach <thebartekbanach.dev@gmail.com>
2024-09-21 16:38:24 +0200

    chore: disable ~/.docker/config.json modifications in on-create.sh to prevent errors on devcontainer startup
    Also, upgrading DevPod to higher version seems to fix it anyway

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -4,5 +4,5 @@ git clone https://github.com/sync-toys/DirectMQ.git /workspace
 chown -R 1000:1000 /workspace
 
 # This is fix to prevent docker pull errors
-rm ~/.docker/config.json
-cp /workspace/.devcontainer/docker-config.json ~/.docker/config.json
+# rm ~/.docker/config.json
+# cp /workspace/.devcontainer/docker-config.json ~/.docker/config.json
