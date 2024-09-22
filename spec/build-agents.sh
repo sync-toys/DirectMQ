4bc28706e6e7b41c849b2a9c4cba5394f8a637f6
Bartek Banach <thebartekbanach.dev@gmail.com>
2024-09-22 13:44:19 +0000

    build: do not remove build caches in spec agents builds

--- spec/build-agents.sh
+++ spec/build-agents.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo Building golang SDK test agent
-cd ./agents/go && rm -rf bin && go build -buildvcs=false -gcflags="all=-N -l" -o bin/go-agent && cd ../../
+cd ./agents/go && go build -buildvcs=false -gcflags="all=-N -l" -o bin/go-agent && cd ../../
 
 echo Building cpp SDK test agent
-cd ./agents/cpp && rm -rf build && cmake -B build -S . -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON && cmake --build build && cd ../../
+cd ./agents/cpp && cmake -B build -S . -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON && cmake --build build && cd ../../
