ab9f96ac7e0848c95f43effbe6e9bd8c0c879992
Bartek Banach <thebartekbanach.dev@gmail.com>
2024-09-23 14:18:51 +0000

    feat: ability to define c++ sdk spec testing agent

--- spec/agent_api/universal_exec.go
+++ spec/agent_api/universal_exec.go
@@ -1,6 +1,7 @@
 package dmqspecagent
 
 type UniversalSpawn struct {
+	AgentType      string
 	NodeID         string
 	ExecutablePath string
 	Arguments      []string
--- spec/agents/agents.go
+++ spec/agents/agents.go
@@ -1,7 +1,6 @@
 package dmqspecagents
 
 import (
-	"fmt"
 	"strconv"
 
 	dmqspecagent "github.com/sync-toys/DirectMQ/spec/agent_api"
@@ -9,22 +8,55 @@ import (
 
 const NO_DEBUGGING = -1
 const DEFAULT_GO_SDK_DEBUG_PORT = 2345
+const DEFAULT_CPP_SDK_DEBUG_PORT = 3456
+
+func GetDefaultDebugPort(agentType string) int {
+	switch agentType {
+	case "go":
+		return DEFAULT_GO_SDK_DEBUG_PORT
+	case "cpp":
+		return DEFAULT_CPP_SDK_DEBUG_PORT
+	default:
+		panic("Unknown agent type, cannot determine default debug port")
+	}
+}
 
 func GolangAgent(nodeID string, debugPort int) dmqspecagent.UniversalSpawn {
 	executablePath := "/directmq/spec/agents/go/bin/go-agent"
 
 	if debugPort == NO_DEBUGGING {
 		return dmqspecagent.UniversalSpawn{
+			AgentType:      "go",
 			NodeID:         nodeID,
 			ExecutablePath: executablePath,
 			Arguments:      []string{},
 		}
 	}
 
-	fmt.Println("debugPort: ", debugPort)
 	return dmqspecagent.UniversalSpawn{
+		AgentType:      "go",
 		NodeID:         nodeID,
 		ExecutablePath: "dlv",
 		Arguments:      []string{"exec", executablePath, "--headless", "--listen=0.0.0.0:" + strconv.Itoa(debugPort), "--api-version=2"},
+	}
+}
+
+func CppAgent(nodeID string, debugPort int) dmqspecagent.UniversalSpawn {
+	executablePath := "/directmq/spec/agents/cpp/build/directmq-sdk-agent"
+
+	if debugPort == NO_DEBUGGING {
+		return dmqspecagent.UniversalSpawn{
+			AgentType:      "cpp",
+			NodeID:         nodeID,
+			ExecutablePath: executablePath,
+			Arguments:      []string{},
+		}
+	}
+
+	return dmqspecagent.UniversalSpawn{
+		AgentType:      "cpp",
+		NodeID:         nodeID,
+		ExecutablePath: "gdbserver",
+		Arguments:      []string{"0.0.0.0:" + strconv.Itoa(debugPort), executablePath},
 	}
 }
