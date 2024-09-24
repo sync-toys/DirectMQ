909f56289a087f342fcf29548b7807b3fc5026d4
Bartek Banach <thebartekbanach.dev@gmail.com>
2024-09-24 10:55:33 +0000

    chore: remove unused devcontainer configuration files

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -2,7 +2,3 @@
 
 git clone https://github.com/sync-toys/DirectMQ.git /workspace
 chown -R 1000:1000 /workspace
-
-# This is fix to prevent docker pull errors
-# rm ~/.docker/config.json
-# cp /workspace/.devcontainer/docker-config.json ~/.docker/config.json
