ef0b3c93534c1cfe90aca4d3dab98e84074ac5c2
Bartek Banach <thebartekbanach.dev@gmail.com>
2024-12-08 17:33:38 +0000

    test: correct dmq C++ SDK spec agent executable path

--- spec/agents/agents.go
+++ spec/agents/agents.go
@@ -42,7 +42,7 @@ func GolangAgent(nodeID string, debugPort int) dmqspecagent.UniversalSpawn {
 }
 
 func CppAgent(nodeID string, debugPort int) dmqspecagent.UniversalSpawn {
-	executablePath := "/directmq/spec/agents/cpp/build/directmq-sdk-agent"
+	executablePath := "/directmq/spec/agents/cpp/build/Debug/directmq-sdk-agent"
 
 	if debugPort == NO_DEBUGGING {
 		return dmqspecagent.UniversalSpawn{
