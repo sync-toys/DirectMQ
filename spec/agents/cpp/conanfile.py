f97d3f7baeaa59246d542b7c6620c001fd597d45
Bartek Banach <thebartekbanach.dev@gmail.com>
2025-01-01 15:27:17 +0000

    build: correct C++ SDK testing agent compilation errors

--- spec/agents/cpp/conanfile.py
+++ spec/agents/cpp/conanfile.py
@@ -7,6 +7,7 @@ class DirectMQTestAgent(ConanFile):
 
     def requirements(self):
         self.requires("nlohmann_json/3.11.3")
+        self.requires("boost/1.86.0")
 
     def build_requirements(self):
         self.build_requires("cmake/[>3.25]")
